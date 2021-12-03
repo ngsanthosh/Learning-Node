81df0fd1d1d77191b2333c1cb5f32173c1ec83b1
ngsanthosh <ngsanthosh657@gmail.com>
2021-12-03 06:48:22 +0530

    Hello world using first virtual server

--- sample.js
+++ sample.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+// console.log("Hello world");
+
+const http = require('http');
+
+http.createServer(function(req,res){
+
+    res.end("Hello world!!, This is a random text!!")
+
+}).listen(9000)
