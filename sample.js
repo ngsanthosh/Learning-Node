9eeb5d6b05bedd0e38c1d37960509e72e1d64239
ngsanthosh <ngsanthosh657@gmail.com>
2021-12-04 06:57:15 +0530

    Routing illustrated

--- sample.js
+++ sample.js
@@ -4,6 +4,16 @@ const http = require('http');
 
 http.createServer(function(req,res){
 
-    res.end("Hello world!!, This is a random text!!")
+    // res.end("Hello world!!, This is a random text!!")
+    // if we need to do routing, then with the help of the req object we can do that
+    if (req.url === '/'){
+        res.end("This is home page")
+    }
+    else if (req.url === '/login'){
+        res.end("This is login page")
+    }
+    else if (req.url === '/signup'){
+        res.end("This is signup page")
+    }
 
 }).listen(9000)
