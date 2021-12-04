949c69ac0032da939ea7ecd7afff4b9ff9db662f
ngsanthosh <ngsanthosh657@gmail.com>
2021-12-04 12:52:17 +0530

    Local modules creation and implemented

---
+++ objinfo.js
@@ -0,0 +1,6 @@
+const db = {
+  name: "Santhosh",
+  age: 19,
+};
+
+module.exports = db;
--- sample.js
+++ sample.js
@@ -1,19 +1,17 @@
 // console.log("Hello world");
+const obj = require('./objinfo')
+const http = require("http");
 
-const http = require('http');
-
-http.createServer(function(req,res){
-
+http
+  .createServer(function (req, res) {
     // res.end("Hello world!!, This is a random text!!")
     // if we need to do routing, then with the help of the req object we can do that
-    if (req.url === '/'){
-        res.end("This is home page")
+    if (req.url === "/") {
+      res.end("This is home page and your name is "+ obj.name);
+    } else if (req.url === "/login") {
+      res.end("This is login page");
+    } else if (req.url === "/signup") {
+      res.end("This is signup page");
     }
-    else if (req.url === '/login'){
-        res.end("This is login page")
-    }
-    else if (req.url === '/signup'){
-        res.end("This is signup page")
-    }
-
-}).listen(9000)
+  })
+  .listen(9000);
