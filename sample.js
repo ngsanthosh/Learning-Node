f9bf292fa6cd58de4f5d1ca4c90cb180b54dc8ce
ngsanthosh <ngsanthosh657@gmail.com>
2021-12-04 15:19:50 +0530

    nodemon illustrated

--- sample.js
+++ sample.js
@@ -7,7 +7,7 @@ http
     // res.end("Hello world!!, This is a random text!!")
     // if we need to do routing, then with the help of the req object we can do that
     if (req.url === "/") {
-      res.end("This is home page and your name is "+ obj.name);
+      res.end("This is home page and your name is, yeah your name is "+ obj.name);
     } else if (req.url === "/login") {
       res.end(`This is login page and your age is ${obj.age}`);
     } else if (req.url === "/signup") {
