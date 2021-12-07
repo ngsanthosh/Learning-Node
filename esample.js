740cc1a7dfd65ed046a0ebb24d2fe6e11e5db4e3
ngsanthosh <ngsanthosh657@gmail.com>
2021-12-07 22:50:41 +0530

    Now we're powerful with express along with nodemon

--- esample.js
+++ esample.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.end("Hello world from express js, now using nodemon!!!");
+  res.end("Hello world from express js, now using nodemon!!! Yeah woohhh");
 });
 
 app.listen(8000, function () {
-  console.log("Connection is started raa");
+  console.log("Connection is started raa dadadadadadaadad");
 });
