825bffbd4f21374df83fa42120de76ff2f127cd3
ngsanthosh <ngsanthosh657@gmail.com>
2021-12-07 22:50:36 +0530

    Played with nodemon and express

--- esample.js
+++ esample.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.end("Hello world from express js");
+  res.end("Hello world from express js, now using nodemon!!!");
 });
 
 app.listen(8000, function () {
