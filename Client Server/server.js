0088a5052b9368077b96f43e4b17cdcced77a13f
ngsanthosh <ngsanthosh657@gmail.com>
2021-12-08 07:09:03 +0530

    Client side printing also writen

--- Client Server/server.js
+++ Client Server/server.js
@@ -2,6 +2,13 @@ const express = require("express")
 
 const app = express()
 
+
+app.get('/', function (req, res) {
+
+    res.send("Hello this text is from api")
+    
+})
+
 app.listen(5000, function(){
     console.log("THe port is listening...");
 })
