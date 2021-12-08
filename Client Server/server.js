f2b4b3ed3b027c404a640c1a4ab656e974f5292d
ngsanthosh <ngsanthosh657@gmail.com>
2021-12-08 07:09:42 +0530

    Prettified

--- Client Server/server.js
+++ Client Server/server.js
@@ -1,14 +1,11 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
+app.get("/", function (req, res) {
+  res.send("Hello this text is from api");
+});
 
-app.get('/', function (req, res) {
-
-    res.send("Hello this text is from api")
-    
-})
-
-app.listen(5000, function(){
-    console.log("THe port is listening...");
-})
+app.listen(5000, function () {
+  console.log("THe port is listening...");
+});
