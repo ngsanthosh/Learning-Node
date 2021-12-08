c701fbc5933869d82af176ebc2cea1dedafbaeb9
ngsanthosh <ngsanthosh657@gmail.com>
2021-12-08 07:16:48 +0530

    Another route to send users as api data

--- Client Server/server.js
+++ Client Server/server.js
@@ -6,6 +6,11 @@ app.get("/", function (req, res) {
   res.send("Hello this text is from api");
 });
 
+app.get("/users", function (req, res) {
+  const users = ["Santhosh", "Naan Gokul", "Naan Tarun"];
+  res.send(users);
+});
+
 app.listen(5000, function () {
   console.log("THe port is listening...");
 });
