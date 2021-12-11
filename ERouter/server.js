9a0e9062f38f04b6ef2435ceb0740d9dd6008792
ngsanthosh <ngsanthosh657@gmail.com>
2021-12-11 12:04:41 +0530

    Using the moduled user functionalities in server.js

--- ERouter/server.js
+++ ERouter/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+const routes = require("./Routes/user");
+
+app.use("/user", routes);
+
 app.listen(5000, function (req, res) {
   console.log("The port is listening to port 5000");
 });
