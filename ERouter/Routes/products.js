89b07d38a79435139af4e00135a737249ab8f021
ngsanthosh <ngsanthosh657@gmail.com>
2021-12-11 12:10:27 +0530

    Completed paths and routes for Products.js

---
+++ ERouter/Routes/products.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const route = express.Router();
+
+route.get("/getproducts", (req, res) => {
+  const products = ["Pepsi", "Coke", "Red bull"];
+  res.send(products);
+});
+
+route.get("/addproducts", (req, res) => {
+  res.send("This is all about adding products page");
+});
