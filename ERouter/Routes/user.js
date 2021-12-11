372f085228ee2bd695788b67b1b4a97b4868ff39
ngsanthosh <ngsanthosh657@gmail.com>
2021-12-11 12:02:19 +0530

    Forget pasword route for user and algo

--- ERouter/Routes/user.js
+++ ERouter/Routes/user.js
@@ -1,3 +1,6 @@
+// The Functionalities of the user is to login to a page or register to a page
+// or else change the password by using forget password link
+
 const express = require("express");
 
 const route = express.Router();
@@ -6,7 +9,10 @@ route.get("/login", (req, res) => {
   res.send("This is login route");
 });
 
-
 route.get("/register", (req, res) => {
   res.send("This is Registration route");
+});
+
+route.get("/forgetpassword", (req, res) => {
+  res.send("This is Resetting password route");
 });
