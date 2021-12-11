414ee180eadd1f45468d9cabc050876386eeacb7
ngsanthosh <ngsanthosh657@gmail.com>
2021-12-11 12:00:30 +0530

    Registration route for user

--- ERouter/Routes/user.js
+++ ERouter/Routes/user.js
@@ -5,3 +5,8 @@ const route = express.Router();
 route.get("/login", (req, res) => {
   res.send("This is login route");
 });
+
+
+route.get("/register", (req, res) => {
+  res.send("This is Registration route");
+});
